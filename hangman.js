373b1689be72b044cf6910858567f16414600a70
petriche <petriche@gmail.com>
2023-05-12 16:15:56 +0200

    Update hangman.js

--- hangman.js
+++ hangman.js
@@ -3,6 +3,9 @@ let displayWord = '_ '.repeat(guessWord.length).trim();
 let guessedLetters = [];
 let lives = 10;
 
+// Array of hangman parts
+let hangmanParts = ['hang1', 'hang2', 'hang3', 'hang4', 'head', 'body', 'leftarm', 'rightarm', 'leftleg', 'rightleg'];
+
 document.getElementById('word').innerText = displayWord;
 document.getElementById('lives').innerText = `Lives: ${lives}`;
 
@@ -15,11 +18,19 @@ function makeGuess() {
             displayWord = guessWord.split('').map(char => guessedLetters.includes(char) ? char : '_').join(' ');
         } else {
             lives--;
+            showHangman();
         }
     }
     document.getElementById('word').innerText = displayWord;
     document.getElementById('lives').innerText = `Lives: ${lives}`;
     checkGameOver();
+}
+
+function showHangman() {
+    let partsToShow = hangmanParts.slice(0, 10 - lives);
+    for (let part of partsToShow) {
+        document.getElementById(part).style.visibility = 'visible';
+    }
 }
 
 function checkGameOver() {
